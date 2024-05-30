b2db03244989bdda6adc9410cce38d1918b19b83
K4sik <kasiks597@gmail.com>
2024-05-30 21:17:17 +0300

    157 Create EventModel

--- dao/src/main/java/greencity/dto/event/EventTime.java
+++ dao/src/main/java/greencity/dto/event/EventTime.java
@@ -1,2 +1,29 @@
-package greencity.dto.event;public class EventTime {
+package greencity.dto.event;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import lombok.*;
+
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@EqualsAndHashCode
+public class EventTime {
+
+    private Long id;
+
+    private boolean allDay;
+
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private Integer dayCount;
+
+//    @ManyToOne
+    private EventModelDto event;
 }
