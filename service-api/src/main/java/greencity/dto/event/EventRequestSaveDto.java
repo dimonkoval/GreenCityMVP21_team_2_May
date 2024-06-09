c18555b246d25ace968e8a32b54e22f63d3d8e24
K4sik <kasiks597@gmail.com>
2024-06-09 18:15:07 +0300

    158 Added annotations to EventRequestSaveDto

--- service-api/src/main/java/greencity/dto/event/EventRequestSaveDto.java
+++ service-api/src/main/java/greencity/dto/event/EventRequestSaveDto.java
@@ -2,7 +2,6 @@ package greencity.dto.event;
 
 import greencity.annotations.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -19,12 +18,12 @@ public class EventRequestSaveDto {
     @Size(message = "Title must be a maximum of 70 characters", max = 70)
     private String title;
 
-//    @NotEmptyEventDateTime
-//    @EventDateAfterOneHour
-//    @ValidSequenceEventDates
-//    @StartBeforeEndTime
-//    @ValidAllDayEvent
-//    @UniqueEventDates
+    @NotEmptyEventDateTime
+    @EventDateAfterOneHour
+    @ValidSequenceEventDates
+    @StartBeforeEndTime
+    @ValidAllDayEvent
+    @UniqueEventDates
     @Size(max = 7, min = 1)
     private List<EventSaveDayInfoDto> daysInfo;
 
