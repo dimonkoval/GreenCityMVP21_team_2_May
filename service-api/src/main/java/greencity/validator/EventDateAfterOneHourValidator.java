3fb50b73e4058abbded0f436a02d3e2ad03b9f88
K4sik <kasiks597@gmail.com>
2024-06-09 16:24:58 +0300

    158 Fixed bug related to the time in minutes

--- service-api/src/main/java/greencity/validator/EventDateAfterOneHourValidator.java
+++ service-api/src/main/java/greencity/validator/EventDateAfterOneHourValidator.java
@@ -5,24 +5,16 @@ import greencity.dto.event.model.EventDayInfo;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public class EventDateAfterOneHourValidator implements ConstraintValidator<EventDateAfterOneHour, List<EventDayInfo>> {
     @Override
     public boolean isValid(List<EventDayInfo> value, ConstraintValidatorContext context) {
-        LocalDate today = LocalDate.now();
-
         //first dateTime of event should be at least in one hour after now
-        if (!value.get(0).getStartDateTime().toLocalDate().isAfter(today)) {
-            if (value.get(0).getStartDateTime().toLocalDate().isBefore(today)) {
-                return false;
-            }
-            int currentHour = LocalTime.now().getHour();
-            if (currentHour >= value.get(0).getStartDateTime().toLocalTime().getHour()) {
-                return false;
-            }
+        ZonedDateTime nowPlusOneHour = ZonedDateTime.now().plusHours(1);
+        if (value.get(0).getStartDateTime().isBefore(nowPlusOneHour)) {
+            return false;
         }
 
         return true;
---
+++ service-api/src/test/java/greencity/validator/EventDateAfterOneHourValidatorTest.java
@@ -0,0 +1,2 @@
+package greencity.validator;public class EventDateAfterOneHourValidatorTest {
+}
